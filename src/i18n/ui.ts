2329b3d63adaf6e1b066495edb7145e3404e7091
Ravachol <ravachol.yang@gmail.com>
2026-01-17 00:58:45 +0800

    update about page

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -95,7 +95,7 @@ export const ui: Record<Language, Translation> = {
   'zh': {
     title: '一只八爪鱼',
     subtitle: '不要温和地走进那个良夜...',
-    description: 'Retypeset是一款基于Astro框架的静态博客主题，中文名为重新编排。本主题以活版印字为设计灵感，通过建立全新的视觉规范，对所有页面进行重新编排，打造纸质书页般的阅读体验，再现版式之美。所见皆为细节，方寸尽显优雅。',
+    description: '神秘的杂食动物（）',
     posts: '文章',
     tags: '标签',
     about: '关于',
